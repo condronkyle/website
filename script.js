ab1b592278b777458811500a5d81d7d1032e78e8
Kyle Condron <kylecondron@kyles-mbp.mynetworksettings.com>
2023-10-13 22:55:14 -0400

    baseball

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-function changeColor() {
-    var colors = ["#FF5733", "#33FF57", "#3357FF"];
-    var currentColor = document.body.style.backgroundColor;
-    var newColor = colors[(colors.indexOf(currentColor) + 1) % colors.length];
-    document.body.style.backgroundColor = newColor;
-}
 
+// JavaScript code for interactivity will go here
